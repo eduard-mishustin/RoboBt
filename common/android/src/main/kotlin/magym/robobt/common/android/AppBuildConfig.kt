793b2de0d6427883c2dd30ff1c09494fc0f48193
Eduard Mishustin <eduard.mishustin@gmail.com>
2024-12-19 15:55:23 +0400

    Add IS_VIDEO_STREAM_ENABLED

--- common/android/src/main/kotlin/magym/robobt/common/android/AppBuildConfig.kt
+++ common/android/src/main/kotlin/magym/robobt/common/android/AppBuildConfig.kt
@@ -2,9 +2,11 @@ package magym.robobt.common.android
 
 object AppBuildConfig {
 
-    const val IS_BLUETOOTH_DEBUG = true
+    const val IS_BLUETOOTH_DEBUG = false
 
     const val IS_HOST = true
+
+    const val IS_VIDEO_STREAM_ENABLED = true
 
     const val BASE_URL = "https://e124-5-178-149-187.ngrok-free.app"
 }
--- repository/video-stream/src/main/java/magym/robobt/video_stream/VideoStreamRepository.kt
+++ repository/video-stream/src/main/java/magym/robobt/video_stream/VideoStreamRepository.kt
@@ -6,7 +6,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOn
+import magym.robobt.common.android.AppBuildConfig.IS_VIDEO_STREAM_ENABLED
 import magym.robobt.video_stream.util.decodeMJPEGStream
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -22,31 +24,37 @@ internal class VideoStreamRepositoryImpl(
     private val client: OkHttpClient,
 ) : VideoStreamRepository {
 
-    override fun connect(onConnectionError: () -> Unit): Flow<Bitmap> = callbackFlow {
-        val request = Request.Builder()
-            .url(VIDEO_STREAM_URL)
-            .build()
-
-        try {
-            client.newCall(request).execute().use { response ->
-                if (response.isSuccessful) {
-                    response.body?.byteStream()?.let { inputStream ->
-                        decodeMJPEGStream(inputStream) { bitmap ->
-                            trySend(bitmap)
-                        }
-                    }
-                } else {
-                    Log.e("Stream", "Connection error: ${response.code}")
-                }
-            }
-        } catch (e: Exception) {
-            closeConnection()
-            e.printStackTrace()
-            onConnectionError.invoke()
+    override fun connect(onConnectionError: () -> Unit): Flow<Bitmap> {
+        if (!IS_VIDEO_STREAM_ENABLED) {
+            return emptyFlow()
         }
 
-        awaitClose { closeConnection() }
-    }.flowOn(Dispatchers.IO)
+        return callbackFlow {
+            val request = Request.Builder()
+                .url(VIDEO_STREAM_URL)
+                .build()
+
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (response.isSuccessful) {
+                        response.body?.byteStream()?.let { inputStream ->
+                            decodeMJPEGStream(inputStream) { bitmap ->
+                                trySend(bitmap)
+                            }
+                        }
+                    } else {
+                        Log.e("Stream", "Connection error: ${response.code}")
+                    }
+                }
+            } catch (e: Exception) {
+                closeConnection()
+                e.printStackTrace()
+                onConnectionError.invoke()
+            }
+
+            awaitClose { closeConnection() }
+        }.flowOn(Dispatchers.IO)
+    }
 
     override fun closeConnection() {
         client.dispatcher.executorService.shutdown()
