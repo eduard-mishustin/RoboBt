62e2b8f0a694c85c7fb666ffdd109d7d5627d0de
Eduard Mishustin <eduard.mishustin@gmail.com>
2024-12-19 16:21:57 +0400

    Remove face detection logic from esp32

--- common/android/src/main/kotlin/magym/robobt/common/android/AppBuildConfig.kt
+++ common/android/src/main/kotlin/magym/robobt/common/android/AppBuildConfig.kt
@@ -6,6 +6,8 @@ object AppBuildConfig {
 
     const val IS_HOST = true
 
+    const val IS_BLUETOOTH_ENABLED = false
+
     const val IS_VIDEO_STREAM_ENABLED = true
 
     const val BASE_URL = "https://e124-5-178-149-187.ngrok-free.app"
