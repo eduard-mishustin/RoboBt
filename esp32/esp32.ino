9987cb3c4c8ac60fdeec9e32e3d3b3a57cbf7a9a
Eduard Mishustin <eduard.mishustin@gmail.com>
2024-12-19 15:27:54 +0400

    Reduce video stream quality

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -72,12 +72,11 @@ void setup() {
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
-    config.frame_size = FRAMESIZE_UXGA;
-    config.pixel_format = PIXFORMAT_JPEG;  // for streaming
-    //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+    config.frame_size = FRAMESIZE_VGA;
+    config.pixel_format = PIXFORMAT_JPEG; // for streaming
     config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
     config.fb_location = CAMERA_FB_IN_PSRAM;
-    config.jpeg_quality = 12;
+    config.jpeg_quality = 30;
     config.fb_count = 1;
 
     // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
